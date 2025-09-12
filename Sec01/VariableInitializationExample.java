27af3103ec2535fce4a9ed832c066c9101b12efe
parksuejin1026 <suejin1026suejin@gmail.com>
2025-09-12 15:16:43 +0900

    java 2weeks

---
+++ Sec01/VariableInitializationExample.java
@@ -0,0 +1,14 @@
+package Ch02.Sec01;
+
+public class VariableInitializationExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value = 0;
+		
+		int result = value + 10;
+		
+		System.out.println(result);
+	}
+
+}

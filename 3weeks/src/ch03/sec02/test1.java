8eacfce01ade7965eed843d0c1598a59a4861580
parksuejin1026 <suejin1026suejin@gmail.com>
2025-09-19 15:39:33 +0900

    3주차 수업

---
+++ 3weeks/src/ch03/sec02/test1.java
@@ -0,0 +1,18 @@
+package ch03.sec02;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		int apple = 123;
+		int bs = 0;
+		int result1 = apple / 10;
+		bs += result1;
+		int result2 = apple % 10;
+		if(result2 != 0)
+			System.out.println(++bs);
+		else
+			System.out.println(bs);
+		System.out.println("바구니의 개수는" +bs+"개다.");
+	}
+
+}

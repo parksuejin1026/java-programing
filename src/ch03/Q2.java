27f60b27506f2defc76dd3e47222449c986ed27d
parksuejin1026 <suejin1026suejin@gmail.com>
2025-09-22 16:02:51 +0900

    3주차 과제

---
+++ src/ch03/Q2.java
@@ -0,0 +1,11 @@
+package ch03;
+
+public class Q2 {
+
+	public static void main(String[] args) {
+		int score = 85;
+		String result = (!(score>90))? "가" :"나";
+		System.out.println(result);
+	}
+
+}
